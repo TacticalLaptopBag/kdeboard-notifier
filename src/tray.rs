4ef25828a3059374089ed5448d144a6338a49e67
Tactical Laptop Bag <tacticallaptopbag@protonmail.com>
2026-04-21 11:09:48 -0400

    Decrease sleep duration

--- src/tray.rs
+++ src/tray.rs
@@ -63,7 +63,7 @@ pub fn run(mut config: Config) -> ! {
         let connected = is_device_connected(config.vendor_id, config.product_id);
         handle.update(|t| t.connected = connected);
 
-        std::thread::sleep(Duration::from_secs(2));
+        std::thread::sleep(Duration::from_secs(1));
     }
 }
 
